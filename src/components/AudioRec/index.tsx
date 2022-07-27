39b88101ad714d45fab5b6400be2ca600d1c5e46
Jussi <jussi.pesonen@adaptek.fi>
2022-07-27 05:04:13 +0300

    Trying to fix voice uploads on server

--- src/components/AudioRec/index.tsx
+++ src/components/AudioRec/index.tsx
@@ -16,18 +16,16 @@ export const AudioRec: Component<{ room: string; id: string }> = (props) => {
       navigator.mediaDevices
         .getUserMedia(constraints)
         .then(function (stream) {
+          console.log('Got the media stream', stream);
           mediaRecorder = new MediaRecorder(stream);
 
           mediaRecorder.onstop = function (e) {
             console.log('data available after MediaRecorder.stop() called.');
 
-            const clipName = prompt('Enter a name for your sound clip');
-
-            const clipLabel = document.createElement('p');
-
             audio.setAttribute('controls', '');
             audio.controls = true;
             blob = new Blob(chunks, { type: 'audio/ogg; codecs=opus' });
+            console.log('blob in then()', blob);
             chunks = [];
             const audioURL = URL.createObjectURL(blob);
             audio.src = audioURL;
@@ -50,15 +48,13 @@ export const AudioRec: Component<{ room: string; id: string }> = (props) => {
   };
   const stopRec = async () => {
     mediaRecorder.stop();
-    setTimeout(async () => {
-      // TODO clean this up, decide on api for api calls
-      console.log('starting upload');
-      let formData = new FormData();
-      console.log('blob', blob);
-      formData.append('document', blob);
-      let res = postVoice(props.id, props.room, formData);
-      console.log(res);
-    });
+    // TODO clean this up, decide on api for api calls
+    console.log('starting upload');
+    let formData = new FormData();
+    console.log('blob', blob);
+    formData.append('document', blob);
+    let res = postVoice(props.id, props.room, formData);
+    console.log(res);
   };
   return (
     <>
